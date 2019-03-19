7197ad7f2fcfa76fef5310f6a784842445664db2
mroleh <oivaniv@n-ix.com>
2019-03-20 01:44:57 +0200

    Added file to add reporting tool

--- src/test/java/SingleFunctionTests/CheckDeviceRidersTC.java
+++ src/test/java/SingleFunctionTests/CheckDeviceRidersTC.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -68,4 +69,8 @@ public class CheckDeviceRidersTC {
 
     }
 
+    @AfterTest
+    public void teardown() {
+        driver.quit();
+    }
 }
--- src/test/java/TestClass.java
+++ src/test/java/TestClass.java
@@ -1,30 +1,38 @@
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
-import io.appium.java_client.android.AndroidElement;
+import io.qameta.allure.*;
+import io.qameta.allure.listener.TestLifecycleListener;
 import org.openqa.selenium.By;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
+import utils.Listeners.TestListener;
 
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.concurrent.TimeUnit;
+
+@Listeners({TestListener.class})
+@Epic("Basic functionality")
+@Feature("FM page")
 
 public class TestClass {
-
 
     public AndroidDriver<MobileElement> driver;
     public WebDriverWait wait;
 
+
     @BeforeMethod
     public void setup() throws MalformedURLException {
         DesiredCapabilities caps = new DesiredCapabilities();
-        caps.setCapability("deviceName", "LG G6");
-        caps.setCapability("udid", "LGH8705f292316");
+        caps.setCapability("deviceName", "Nexus 5");
+        caps.setCapability("udid", "03787176d024355e");
         caps.setCapability("platformName", "Android");
-        caps.setCapability("platformVersion", "8.0");
+        caps.setCapability("platformVersion", "6.0.1");
         caps.setCapability("appPackage", "com.cardo.smartset");
         caps.setCapability("appActivity", "com.cardo.smartset.ui.activities.PrivacyPolicyActivity");
         caps.setCapability("noReset", "true");
@@ -32,56 +40,66 @@ public class TestClass {
         wait = new WebDriverWait(driver, 10);
     }
 
-    @Test
-    public void checkBatteryLife() throws InterruptedException {
 
-        int count = 0;
-        int passedCount = 0;
-        int failedCount = 0;
+    @Test(priority = 0, description = "Verify FM page open functionality")
+    @Severity(SeverityLevel.BLOCKER)
+    @Description("Verify FM page opem and close functionality.")
+    @Story("FM page")
 
-        do {
-            driver.launchApp();
 
-            //System.out.println("Device connected");
-            driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
-            //System.out.println("Added timeout");
+    //*****************Test Methods******************************8
+    @Step("Press FM page button..")
+    public void openFmPage() throws InterruptedException {
+        MobileElement fmPageButton = driver.findElement(By.id("com.cardo.smartset:id/radio_button"));
+        fmPageButton.click();
+    }
 
-            //Click on Quick Access button
-            wait.until(ExpectedConditions.visibilityOfElementLocated
-                    (By.id("com.cardo.smartset:id/intercom_btn"))).click();
+    @Step("Press Play FM button..")
+    public void pressPlayFMbutton() throws InterruptedException {
+        MobileElement startFMButton = driver.findElement(By.id("activity_radio_start_stop_radio_btn"));
+        startFMButton.click();
+    }
 
-            if (!driver.findElements(By.id("com.cardo.smartset:id/fragment_intercom_channels_list")).isEmpty()) {
-                System.out.println("Test passed");
-                passedCount++;
-            } else {
-                System.out.println("Test failed");
-                failedCount++;
-            }
+    @Step("Press Stop FM button..")
+    public void pressStopFmButton() {
+        MobileElement stopFMButton = driver.findElement(By.id("activity_radio_start_stop_radio_btn"));
+        stopFMButton.click();
+    }
 
-            driver.closeApp();
-            //System.out.println("App is closed");
-            count++;
-            System.out.println("Total count is: " + count);
+    @Step("Press X button..")
+    public void pressClosePageButton() {
+        MobileElement closeFmPageButton = driver.findElement(By.id("material_design_toolbar_left_icon"));
+        closeFmPageButton.click();
+    }
 
-        } while (count < 100);
-        System.out.println("Passed test: " + passedCount + "\nFailed tests: " + failedCount);
 
-//        //Check is value is set
-//        System.out.println(deviceName);
+
+
+//        //System.out.println("Device connected");
+//        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+//        //System.out.println("Added timeout");
 //
-//        if (deviceName.equals("FREECOM4+")) {
-//            System.out.println("Test passed");
-//            passedCount++;
-//        } else {
-//            System.out.println("Test failed");
-//            failedCount++;
-//        }
+//        //Click on Radio button
+//        wait.until(ExpectedConditions.visibilityOfElementLocated
+//                (By.id("com.cardo.smartset:id/radio_button"))).click();
+//
+//        //Click on Start button
+//        wait.until(ExpectedConditions.visibilityOfElementLocated
+//                (By.id("com.cardo.smartset:id/activity_radio_start_stop_radio_btn"))).click();
+//
+//        //Click on Stop button
+//        wait.until(ExpectedConditions.visibilityOfElementLocated
+//                (By.id("com.cardo.smartset:id/activity_radio_start_stop_radio_btn"))).click();
+//
+//        //Click X button
+//        wait.until(ExpectedConditions.visibilityOfElementLocated
+//                (By.id("com.cardo.smartset:id/material_design_toolbar_left_icon"))).click();
 
-        driver.closeApp();
-        //System.out.println("App is closed");
-        // count++;
-//        System.out.println("Total count is: " + count);
 
+
+    @AfterTest
+    public void teardown() {
+        driver.quit();
     }
 
 
@@ -115,3 +133,14 @@ public class TestClass {
 //Click on Quick Access button
 //wait.until(ExpectedConditions.visibilityOfElementLocated
 //(By.id("com.cardo.smartset:id/riding_mode_btn"))).click();
+
+//        //Check is value is set
+//        System.out.println(deviceName);
+//
+//        if (deviceName.equals("FREECOM4+")) {
+//            System.out.println("Test passed");
+//            passedCount++;
+//        } else {
+//            System.out.println("Test failed");
+//            failedCount++;
+//        }
